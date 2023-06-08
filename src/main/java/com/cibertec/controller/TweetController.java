8684d033c54f4c062ec0b172d3418a819a62f3de
Neiser <Neiser@gmail.com>
2023-06-07 22:11:14 -0500

    mapeo, paquetes y agregar nuevo tweet

---
+++ src/main/java/com/cibertec/controller/TweetController.java
@@ -0,0 +1,65 @@
+package com.cibertec.controller;
+
+import java.time.Clock;
+import java.time.LocalDateTime;
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.cibertec.models.Tweet;
+import com.cibertec.service.TweetService;
+
+@Controller
+@RequestMapping("/tweet")
+public class TweetController {
+	
+	@Autowired
+	private TweetService serviTweet;
+	
+	@RequestMapping("/lista")
+	public String inicio(Model model) {
+
+		List<Tweet> data = serviTweet.listarTweet();
+		model.addAttribute("listaTweet", data);
+		return "tweet";
+	}
+	
+	@RequestMapping("/nuevo")
+	public String registro(Model model) {
+		Tweet objTweet = new Tweet();
+		objTweet.setId(0);
+		model.addAttribute("tweet", objTweet);
+		return "register";
+	}
+
+	@RequestMapping("/guardar")
+	public String grabar(@ModelAttribute("tweet") 
+	Tweet tweet, BindingResult result, Model model,
+	RedirectAttributes redirect) {
+		try {
+			Tweet objTweet = new Tweet();
+			
+			objTweet.setAuthor("hola");
+			Date defaultValue = new Date(0);
+			objTweet.setTime(defaultValue);
+			objTweet.setContent("holaaa");
+
+			serviTweet.guardar(objTweet);
+
+		} catch (Exception e) {
+			redirect.addAttribute("mensaje", "Ocurrió un error al intentar guardar tweet!");
+			e.printStackTrace();
+		}
+		
+		return "redirect:/tweet/lista";
+	}
+}
---
+++ src/main/java/com/cibertec/models/Tweet.java
@@ -0,0 +1,61 @@
+package com.cibertec.models;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tweet")
+public class Tweet {
+
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id_tweet")
+    long id;
+
+	@Column(name="usu_tweet")
+    String author;
+
+    @Column(name="hora_tweet")
+    Date time;
+
+    @Column(name="content_tweet")
+    String content;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public Date getTime() {
+		return time;
+	}
+
+	public void setTime(Date time) {
+		this.time = time;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	} 
+}
