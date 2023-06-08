6d9e805ed809f3cd964033904b74c44441cc62ad
Víctor Raúl Quispe Imata <iamvrquim@gmail.com>
2023-06-07 22:21:02 -0500

    Arreglos del Controller

--- src/main/java/com/cibertec/controllers/MainController.java
+++ src/main/java/com/cibertec/controllers/MainController.java
@@ -1,23 +1,30 @@
 package com.cibertec.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.cibertec.models.Tweet;
 import com.cibertec.repositories.TweetDao;
+import com.cibertec.services.TweetService;
 
 @Controller
 public class MainController {
 
 	@Autowired
 	private TweetDao tweetRepository;
+	@Autowired
+	private TweetService tweetService;
 	
 	@GetMapping("/login")
 	public String login() {
